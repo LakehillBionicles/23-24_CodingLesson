801cd0a8095bc86a859b1f2278aa14346c610487
Connor <csullivan@lakehillprep.org>
2023-09-15 10:59:39 -0500

    Connor's Tutorial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sullivan/sullivanTeleOp.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.sullivan;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp
+public class sullivanTeleOp extends LinearOpMode {
+    sullivanHardwareMap robot = new sullivanHardwareMap();
+
+    public void runOpMode() {
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            robot.fpd.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x);
+            robot.bpd.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
+            robot.fsd.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
+            robot.bsd.setPower(-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x);
+
+        }
+    }
+}
