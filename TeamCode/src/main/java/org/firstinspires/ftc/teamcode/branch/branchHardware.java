9378d9a8136bf81d2b0e4b750336c30f5cfd9f05
catherinebranch <catherinebranch@lakehillprep.org>
2023-09-19 13:57:48 -0500

    Catherine 9/19 1:57

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/branch/branchHardware.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.branch;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class branchHardware extends LinearOpMode {
+    public DcMotor fpd = null, bpd = null, fsd = null, bsd= null;
+
+    HardwareMap hwMap = null;
+    public branchHardware(){}
+    @Override
+    public void runOpMode(){}
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+    fpd = hwMap.get(DcMotor.class,"fpd");
+    bpd = hwMap.get(DcMotor.class,"bpd");
+    fsd = hwMap.get(DcMotor.class,"fsd");
+    bsd = hwMap.get(DcMotor.class,"bsd");
+
+    fpd.setDirection(DcMotorSimple.Direction.REVERSE);
+    bpd.setDirection(DcMotorSimple.Direction.FORWARD);
+    fsd.setDirection(DcMotorSimple.Direction.REVERSE);
+    fsd.setDirection(DcMotorSimple.Direction.FORWARD);
+
+    fpd.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    bpd.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    fsd.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    bsd.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    fpd.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    fsd.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    bpd.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    bsd.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+    }
+}
